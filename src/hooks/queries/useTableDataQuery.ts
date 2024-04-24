fab2a860b02e3060f0d7b5bbd331685b766262d0
rachel-park-dev <borapark13@gmail.com>
2024-04-24 17:21:49 +0900

    fix fetch url.

--- src/hooks/queries/useTableDataQuery.ts
+++ src/hooks/queries/useTableDataQuery.ts
@@ -1,17 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
-import { TTableData } from "../../@types/tableType";
+import {useQuery} from "@tanstack/react-query";
+import {TTableData} from "../../@types/tableType";
 
 const QUERY_KEY = ["tableData"];
 export const useTableDataQuery = () => {
-  const { data } = useQuery<TTableData[]>({
+  const {data} = useQuery<TTableData[]>({
     queryKey: QUERY_KEY,
     queryFn: async () => {
-      const response = await fetch("http://localhost:5173/data/data.json").then(
+      const response = await fetch("/data/data.json").then(
         (res) => res.json()
       );
       return response.data;
     },
   });
 
-  return { data };
+  return {data};
 };
